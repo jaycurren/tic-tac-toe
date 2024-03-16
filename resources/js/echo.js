b41307b30c684eeed15d8765f21eb70cbadf670c
jasoncurrent <j.curren@tshirtandsons.com>
2024-03-16 23:15:54 +0000

    serverside config update

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -3,15 +3,28 @@ import Echo from 'laravel-echo';
 import Pusher from 'pusher-js';
 window.Pusher = Pusher;
 
-window.Echo = new Echo({
-    broadcaster: "reverb",
-    cluster: import.meta.env.VITE_PUSHER_CLUSTER,
-    disableStats: true,
-    key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: import.meta.env.VITE_REVERB_HOST,
-    wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
-    wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
-    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
-    enabledTransports: ['ws', 'wss'],
-    encrypted: true
-});
+let options = {};
+
+if (import.meta.env.APP_ENV === "local") {
+    options = {
+        broadcaster: "reverb",
+        cluster: import.meta.env.VITE_PUSHER_CLUSTER,
+        disableStats: true,
+        key: import.meta.env.VITE_REVERB_APP_KEY,
+        wsHost: import.meta.env.VITE_REVERB_HOST,
+        wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
+        wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
+        forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
+        enabledTransports: ['ws', 'wss'],
+        encrypted: true
+    };
+} else {
+    options = {
+        broadcaster: "pusher",
+        key: import.meta.env.VITE_PUSHER_APP_KEY,
+        cluster: import.meta.env.VITE_PUSHER_CLUSTER,
+        forceTLS: true
+    };
+}
+
+window.Echo = new Echo(options);
